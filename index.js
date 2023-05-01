c0c23a17886f0e64f02857b94528d95e6d56bd30
unknown <ajoy9532@gmail.com>
2023-05-02 00:54:12 +0600

    added chefs json file

--- index.js
+++ index.js
@@ -3,4 +3,25 @@ const app = express();
 const cors = require("cors");
 const port = process.env.port || 5000;
 
+const chefs = require("./data/chefs.json");
+
 app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("spicy club running");
+});
+
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
+});
+
+app.get("/chefs/:id", (req, res) => {
+  const _id = req.params.id;
+  const selectedChefs = chefs.find((chef) => +chef.id === +_id);
+  console.log(selectedChefs);
+  res.send(selectedChefs);
+});
+
+app.listen(port, () => {
+  console.log(`spicy club running on port : ${port}`);
+});
